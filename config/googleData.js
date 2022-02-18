2c1c3525dc7366a9786d71ae40de4a71a2f47390
ideaBlogging <mohit.mahawar2011@gmail.com>
2022-02-18 15:23:42 +0530

    first repo

---
+++ config/googleData.js
@@ -0,0 +1,2 @@
+module.exports.clientId = "499204611007-t9sfj9nh52lgfcsk4cijnqh38usndprc.apps.googleusercontent.com";
+module.exports.clientSecret = "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";
---
+++ config/monkoKEY.js
@@ -0,0 +1,1 @@
+module.exports = "mongodb+srv://mohit:mohitmahawar@cluster0.kbujs.mongodb.net/arya?retryWrites=true&w=majority";
